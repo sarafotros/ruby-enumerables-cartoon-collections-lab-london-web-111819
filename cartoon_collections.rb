2d9d5b64511472a83534e765254e2358219d0f97
Sara Fotros <sarafotros1@yahoo.com>
2019-11-15 13:46:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves(array)
-   array =[]
+   array =["Doc", "Dopey", "Bashful", "Grumpy"]
+   array.each do |array|
+     
+   
 end
 
 def summon_captain_planet# code an argument here
