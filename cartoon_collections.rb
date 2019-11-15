70586f18075d7ab655f35e0225c5a5df6e45fe6f
Sara Fotros <sarafotros1@yahoo.com>
2019-11-15 14:08:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+   array =["Doc", "Dopey", "Bashful", "Grumpy"]
+   
+   array.each_with_index do {|array , index|
+     puts index array
+    end
+    
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+planeteer_calls.map{|item|item.caplitalize}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls()
   # Your code here
 end
 
